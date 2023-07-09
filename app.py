99906ec678d71a0d7a56cb3917396e5dbaf59eaf
pediak <pediak@deloitte.com>
2023-07-09 15:04:19 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import requests
+import uuid
+import pygamp as pg
+from decimal import Decimal
+
+
+# Fetch the exchange rate from the National Bank of Ukraine API
+response = requests.get(
+    'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+exchange_data = response.json()
+currency_rate = None
+
+
+def send_event(currency_name, currency_rate,):
+    label = 'UAH/' + currency_name
+    # Send the UAH/USD ratio as a custom event to Google Analytics
+    pg.event(
+        cid='276331731',
+        property_id='394362215',
+        category='Currency',
+        action='Exchange Rate',
+        label=label,
+        value=currency_rate,
+        non_interactive=0
+    )
+    # Confirm successful execution
+    print(
+        f'Successfully sent {currency_name}  exchange rate to Google Analytics: {currency_rate}')
+
+for rate in exchange_data:
+    currency_name = rate['cc']
+    if currency_name in ('USD', 'EUR', 'PLN'):
+        currency_rate = rate['rate']
+        print(currency_name, ':', currency_rate)
+        if currency_rate is None:
+            # Handle error if exchange rate not found
+            raise Exception(f'Unable to retrieve {currency_name} exchange rate.')
+        send_event (currency_name, currency_rate)
+        continue
+
+
